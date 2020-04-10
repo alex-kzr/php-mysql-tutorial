8ba46a67596f209d9ee8b91dd4cc70cec4c3dd35
Alex-SK <aleksey.kozyura@gmail.com>
2020-04-10 22:13:42 +1000

    Create project

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+    echo 'hello from PHP';
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial</title>
+</head>
+<body>
+
+    <h1><?php echo 'Hello again'; ?></h1>
+    
+</body>
+</html>
