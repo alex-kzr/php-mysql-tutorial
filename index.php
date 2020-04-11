59db14cfe7a420862a8f2d209d0dd2731714357e
Alex-SK <aleksey.kozyura@gmail.com>
2020-04-11 20:53:07 +1000

    Add booleans & comparisons

--- index.php
+++ index.php
@@ -1,41 +1,37 @@
 <?php 
     
-    echo 'loops:';
+    echo 'booleans & comparisons:';
+    echo '<br>';
+    echo true; // '1'
+    echo '<br>';
+    echo false; // empty string
     echo '<br>';
 
-    $ninjas = ['shaun', 'ryu', 'yoshi'];
-    for($i = 0; $i < count($ninjas); $i++){
-        echo $ninjas[$i] . ' /';
-    }
-    echo '<br>';
-
-    foreach($ninjas as $ninja){
-        echo $ninja . ' |';
-    }
-    echo '<br>';
+    // numbers
+	// echo 5 < 10;
+	// echo 5 > 10;
+	// echo 5 == 10;
+	// echo 10 == 10;
+	// echo 5 != 10;
+	// echo 5 <= 5;
+    // echo 5 >= 5;
     
-    $products = [
-		['name' => 'shiny star', 'price' => 20],
-		['name' => 'green shell', 'price' => 10],
-		['name' => 'red shell', 'price' => 15],
-		['name' => 'gold coin', 'price' => 5],
-		['name' => 'lightning bolt', 'price' => 40],
-		['name' => 'banana skin', 'price' => 2]
-    ];
+    // strings
+	//echo 'shaun' < 'yoshi'; // true - comparison 1st char position
+	//echo 'shaun' > 'yoshi'; // false
+	//echo 'shaun' > 'Shaun'; // true
+	//echo 'mario' == 'mario'; // true
+	//echo 'mario' == 'Mario'; // false
     
-    foreach($products as $product){
-        echo $product['name'] . ' - ' . $product['price'];
-        echo '<br>';
-    }
+    echo 'loose vs strict comparisons:';
     echo '<br>';
+    //echo 5 == '5'; // true
+	//echo 5 === '5'; // false
+	//echo 5 === 5; // true
 
-    $i = 0;
-    while($i < count($products)){
-        echo $products[$i]['name'];
-        echo '<br>';
-        $i++;
-    }
-
+    //echo true == 1; // true
+    //echo false == "" // true
+	//echo true === 1; // false
 
 ?>
 
@@ -46,12 +42,6 @@
     <title>PHP Tutorial</title>
 </head>
 <body>
-    <h1>Products</h1>
-    <ul>
-        <?php foreach($products as $product){ ?>
-            <h3><?php echo $product['name']; ?></h3>
-            <p>$<?php echo $product['price']; ?></p>
-        <?php } ?>
-    </ul>
+    
 </body>
 </html>
