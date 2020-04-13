9d6cc2832b011d441f62089d6133732fae880749
Alex-SK <aleksey.kozyura@gmail.com>
2020-04-13 16:10:17 +1000

    Add form

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,6 @@
-<footer class="section">
-    <div class="center grey-text">Copyright 2020 Ninja Pizzas</div>
-</footer>
+    <footer class="section">
+        <div class="center grey-text">Copyright 2020 Ninja Pizzas</div>
+    </footer>
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
