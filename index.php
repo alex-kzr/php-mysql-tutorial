fd7cad70fe7d1d07c8938613d6cc44c3d2056095
Alex-SK <aleksey.kozyura@gmail.com>
2020-04-13 14:53:31 +1000

    Clear project

--- index.php
+++ index.php
@@ -1,24 +1,10 @@
 <?php 
+  
+  
     
-    echo 'include:';
-    echo '<br>';
-
-    include('some_other_php_file.php');
-    include('some_other_php_file2.php'); // do not crash down code if file missing
-
-
-    echo 'require:';
-    echo '<br>';
-
-    require('some_other_php_file.php');
-    // require('some_other_php_file2.php'); // chash down code if file missing
-
-    echo 'end of php';
-
-    
-
 ?>
 
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -26,8 +12,6 @@
     <title>PHP Tutorial</title>
 </head>
 <body>
-        <?php include('content.php') ?>
-        <?php include('content.php') ?>
-        <?php include('content.php') ?>
+        
 </body>
 </html>
--- some_other_php_file.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    
-    $ninjas = ['alex', 'ryu', 'yoshi'];
-    echo $ninjas[1] . '<br>';
-    
-
-?>
