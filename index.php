2ee19140fb8f8b97142540e6aaf1fe0908fbf97d
Alex-SK <aleksey.kozyura@gmail.com>
2020-04-13 15:12:04 +1000

    Add project header & footer

--- index.php
+++ index.php
@@ -6,12 +6,8 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Tutorial</title>
-</head>
-<body>
-        
-</body>
+
+    <?php include('templates/header.php') ?>
+    <?php include('templates/footer.php') ?>
+
 </html>
