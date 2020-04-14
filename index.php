e556b92eec2702ac7b3b65e6a5038aae32bbca69
Alex-SK <aleksey.kozyura@gmail.com>
2020-04-14 12:15:09 +1000

    Connecting to a DB

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php 
   
-  
+    // connect to DB
+    $conn = mysqli_connect('localhost', 'ninja', 'Test123456@$*', 'ninja_pizza');
+
+    // check connection
+    if(!$conn){
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
     
 ?>
 
