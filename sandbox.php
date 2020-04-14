c0f27e1544267cb184318b0d2b4979aebf9938d8
Alex-SK <aleksey.kozyura@gmail.com>
2020-04-14 19:11:49 +1000

    Add ternary operator

---
+++ sandbox.php
@@ -0,0 +1,22 @@
+<?php
+
+    echo 'ternary operators:';
+    echo '<br>';
+
+    $score = 50;
+    
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial</title>
+</head>
+<body>
+    
+    <p><?php echo $score > 40 ? 'high score!' : 'low scrore :('; ?></p>
+
+</body>
+</html>
